6216f5ca19f0c73485d2be46639a495e674ff976
foobar404 <austinthemichaud@gmail.com>
2019-11-01 18:51:38 -0400

    enable cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors")
 const books_route = require("./books/books_route")
 const server = express();
 
 server.use(helmet())
+server.use(cors())
 server.use("/books", books_route)
 
 module.exports = server;
