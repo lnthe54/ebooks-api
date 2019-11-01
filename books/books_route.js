fa6244c7dd14924a1f20e9a1c5772b4b81022369
foobar404 <austinthemichaud@gmail.com>
2019-11-01 18:06:07 -0400

    include first 1000 books add ?page

--- books/books_route.js
+++ books/books_route.js
@@ -1,28 +1,34 @@
 const router = require("express").Router();
 const fs = require('fs');
 
-const books = {
-    "aesops fables": {
-        title: "Aesop's Fables",
-        author: "London: Cassell, Petter and Galpin, 1874.",
-        cover: "http://www.read.gov/books/images/aesops_fables_m.jpg"
-    }
-}
-
-
-
+const books = require("./books.json")
 
 router.get("/", (req, res) => {
-    res.json(Object.keys(books))
+    let page = req.query.page
+    if (!page) {
+        res.json(books)
+    } else {
+        let temp = {}
+        let count = 0;
+
+        for (let key in books) {
+            if (count >= Number(page) * 50 && count <= (Number(page) * 50) + 50) {
+                temp[key] = books[key]
+            }
+            count++
+        }
+        res.status(200).json(temp)
+    }
+
 })
 
 router.get("/:name", (req, res) => {
     let name = req.params.name.toLowerCase();
-    let path = process.cwd() + "/books/files/" + name + ".txt";
+    let path = process.cwd() + "/books/files/" + books[name].file;
 
     fs.readFile(path, 'utf8', function (err, content) {
         if (err) {
-            res.status(404).send("the book by the name of " + name + " was not found")
+            res.status(404).send("the book by the name of " + name + " was not found").end()
         }
 
         res.status(200).json({ ...books[name], content })
